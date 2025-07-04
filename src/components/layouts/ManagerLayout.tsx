3b6efa7d0ac6c0629259c04828c5ec2f2fb25181
PrabhaniJeewanthika <iit22049@std.uwu.ac.lk>
2025-07-04 17:36:53 +0530

    Feat: ManagerLayout Page

---
+++ src/components/layouts/ManagerLayout.tsx
@@ -0,0 +1,22 @@
+import { Box, Flex, HStack, Text, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { Outlet } from 'react-router';
+import ManagerSidebar from '../ui/ManagerSidebar';
+import Header from '../ui/Header';
+
+
+const ManagerLayout = () => {
+    return (
+        <Flex height="100vh" flexDirection="row">
+            <ManagerSidebar />
+            <Box ml="250px" flex="1" bg="gray.50">
+                <Header />
+                <div className='h-full px-6 py-3'>
+                    <Outlet />
+                </div>
+            </Box>
+        </Flex>
+    );
+};
+
+export default ManagerLayout;
