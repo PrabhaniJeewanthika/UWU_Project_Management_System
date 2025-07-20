6e7bce3fe05eb2ff4355dfe96c40333251d5f871
GehanLiyanage2002 <iit22002.std.uwu.ac.lk>
2025-07-20 12:36:35 +0530

    CoordinatorScheduleMeeting

---
+++ src/components/pages/CoordinatorPages/CoordinatorScheduleMeeting.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoordinatorScheduleMeeting = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CoordinatorScheduleMeeting;
